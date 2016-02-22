d80faaeeb1774f79d1093c123c9bd93198321570
Shena Yoshida <sheyoshida@gmail.com>
2016-02-22 17:02:05 -0500

    fixed table view warning

--- grocery-objc/grocery-objc/RecipesTableViewController.m
+++ grocery-objc/grocery-objc/RecipesTableViewController.m
@@ -31,8 +31,8 @@
     [self.tableView registerNib:nib forCellReuseIdentifier:@"recipeIdentifier"];
     
     // set height of cell to adjust to text
-    //    self.tableView.rowHeight = UITableViewAutomaticDimension;
-    //    self.tableView.estimatedRowHeight = 50;
+        self.tableView.rowHeight = UITableViewAutomaticDimension;
+        self.tableView.estimatedRowHeight = 50;
     
     self.tableView.backgroundColor = [UIColor colorWithRed:248.0/255.0 green:213.0/255.0 blue:199.0/255.0 alpha:1];
     self.tableView.backgroundView.backgroundColor = [UIColor colorWithRed:248.0/255.0 green:213.0/255.0 blue:199.0/255.0 alpha:1];
@@ -59,7 +59,6 @@
     }
     
     //make a call to the recipe API
-    
     [self fetchAPIData];
     
     
@@ -154,51 +153,5 @@
     destViewController.recipeID = [self.recipes[indexPath.row] objectForKey:@"id"];
     
 }
-
-
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
 
 @end
